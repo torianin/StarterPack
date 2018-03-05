39ab422ea65bd670ec451b05cdb131bc4ae4d024
Torianin <tori@robert-i.com>
2018-03-05 23:55:15 +0100

    Add developer apps

---
+++ Developer.sh
@@ -0,0 +1,28 @@
+# -----------
+# Developer
+# -----------
+
+# -----------
+# Brew
+# -----------
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# -----------
+# Wget
+# -----------
+brew install wget
+
+# -----------
+# Oh My Zsh
+# -----------
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# -----------
+# iTerm2
+# -----------
+brew cask install iterm2
+
+# -----------
+# SourceTree
+# -----------
+brew cask install sourcetree
