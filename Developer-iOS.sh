facb88dfbda186c22f8a63844c258e7acad56309
Torianin <tori@robert-i.com>
2018-04-22 12:54:21 +0200

    Add mas, add ruby Xcode installer from AppStore

--- Developer-iOS.sh
+++ Developer-iOS.sh
@@ -3,6 +3,21 @@
 # -----------
 
 # -----------
+# Xcode
+# -----------
+ruby ./Xcode.rb
+
+# -----------
+# iTerm2
+# -----------
+brew cask install iterm2
+
+# -----------
+# SourceTree
+# -----------
+brew cask install sourcetree
+
+# -----------
 # CocoaPods
 # -----------
 sudo gem install cocoapods
--- Developer.sh
+++ Developer.sh
@@ -8,6 +8,11 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # -----------
+# Mas
+# -----------
+brew install mas
+
+# -----------
 # Wget
 # -----------
 brew install wget
@@ -16,13 +21,3 @@ brew install wget
 # Oh My Zsh
 # -----------
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# -----------
-# iTerm2
-# -----------
-brew cask install iterm2
-
-# -----------
-# SourceTree
-# -----------
-brew cask install sourcetree
